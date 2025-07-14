255e1e3b4c423ceeca2e5e881cd2b67e11e24b33
HungHong <Honghuyhung46@gmail.com>
2025-07-14 02:11:54 -0400

    Fix, top bar theme issue
    Dynamically change to user current theme

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,10 +1,20 @@
 //root layout
 import { Stack } from 'expo-router';
+import { useThemeStyles } from '../constants/Styles';
 
 export default function RootLayout() {
+  const { colors } = useThemeStyles();
+
   return (
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+        },
+        headerTintColor: colors.text,
+        headerShadowVisible: false,
+        headerTitle: '',
+      }}
+    />
   );
 }
