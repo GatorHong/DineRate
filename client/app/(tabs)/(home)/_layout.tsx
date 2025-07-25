2dbc090a572c46a366871b9b69670ec508eb14a7
HungHong <Honghuyhung46@gmail.com>
2025-07-24 23:09:46 -0400

    Fix white page in _layout, home

--- client/app/(tabs)/(home)/_layout.tsx
+++ client/app/(tabs)/(home)/_layout.tsx
@@ -1,9 +1,7 @@
 import { Stack } from 'expo-router';
-import { useThemeStyles } from '.../../../constants/Styles';
-import {useLocalSearchParams} from 'expo-router';
 
 export default function RootLayout() {
-    return (
-        <Stack/>
-    );
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
